4553af999b9cd1af0dc283a8e5631924f54d1a42
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-01 13:04:37 +0530

    Rendering todo in DOM

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 //this array holds list of todo items
 let todoItems = [];
 
+
+function renderTodo(todo) {
+    
+    // List of todo items
+    const list = document.querySelector(".js-todo-list")
+
+    // If  todo.checked is true so assign "done" otherwise an assign empty string
+    const isChecked = todo.checked ? "done" : "";
+
+    // Create node of list 
+    const node = document.createElement("li");
+
+    //  set the class attribute on node element
+    node.setAttribute("class", `todo-item ${isChecked}`);
+
+    //  set the data-key attribute on node element
+    node.setAttribute("data-key", todo.id);
+
+    node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+    list.append(node);
+}
+
 // function will creates object of TO DO items base on what entered in input text and push it to todoItems array with unique id name created with Date.now().
 
 function addTodo(text) {
@@ -11,6 +41,7 @@ function addTodo(text) {
     };
 
     todoItems.push(todo)
+    renderTodo(todo)
     console.log(todoItems);
 } 
 
@@ -36,3 +67,5 @@ form.addEventListener("submit", event => {
     }
 
 })
+
+
